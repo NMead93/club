02c16a54bf778ddc33c34dbd551004a183b1389a
Nicholas Mead <Guest@epicodus-86.local>
2017-04-07 10:24:43 -0700

    update add player form

--- src/app/add-player/add-player.component.ts
+++ src/app/add-player/add-player.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../admin.service';
+import { Player } from '../player.model';
 
 @Component({
   selector: 'app-add-player',
   templateUrl: './add-player.component.html',
-  styleUrls: ['./add-player.component.css']
+  styleUrls: ['./add-player.component.css'],
+  providers: [AdminService]
 })
 export class AddPlayerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private adminService: AdminService) { }
 
   ngOnInit() {
+  }
+
+  addPlayer(name, url, position, country, average, hr) {
+    let stats: number[] = [average, hr];
+    var newPlayer: Player = new Player(url, name, position, country, stats);
+    this.adminService.
   }
 
 }
--- src/app/admin.service.ts
+++ src/app/admin.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Player } from './player.model';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Injectable()
 export class AdminService {
+  players: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private angularFire: AngularFire) {
+    this.players = angularFire.database.list('players');
+  }
+
+  addPlayer(newPlayer: Player) {
+    this.players.push(newPlayer);
+  }
 
 }
